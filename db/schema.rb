43781c48f597d510a44a57123b949cdbdab64f30
Paul Wenig <pwenig@gmail.com>
2018-09-16 16:30:07 -0600

    Fix migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,55 +10,55 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170623233700) do
+ActiveRecord::Schema.define(version: 2017_06_23_233700) do
 
   create_table "attends", force: :cascade do |t|
-    t.integer  "attendee_id"
-    t.integer  "attended_event_id"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.integer "attendee_id"
+    t.integer "attended_event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["attended_event_id"], name: "index_attends_on_attended_event_id"
     t.index ["attendee_id", "attended_event_id"], name: "index_attends_on_attendee_id_and_attended_event_id", unique: true
     t.index ["attendee_id"], name: "index_attends_on_attendee_id"
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string   "name"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string   "user_id"
-    t.text     "body"
-    t.integer  "event_id"
+    t.string "user_id"
+    t.text "body"
+    t.integer "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_comments_on_event_id"
   end
 
   create_table "events", force: :cascade do |t|
-    t.string   "title"
-    t.string   "description"
+    t.string "title"
+    t.string "description"
     t.datetime "date"
-    t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "picture"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.string   "address"
-    t.integer  "category_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "picture"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
+    t.integer "category_id"
     t.index ["category_id"], name: "index_events_on_category_id"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
