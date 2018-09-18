b8e94eca3eb3b63e651b3cbda7f35eb4c43dad94
Paul Wenig <pwenig@gmail.com>
2018-09-18 14:14:12 -0600

    Comment out featured events

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,16 +2,17 @@
 
 class PagesController < ApplicationController
   def index
-    # Test geocoding services locally by hardcoding latitude and longitude
-    if Rails.env.development? || Rails.env.test?
-      visitor_latitude = 34.05223
-      visitor_longitude = -118.24368
-    else
-      visitor_latitude = request.location.latitude
-      visitor_longitude = request.location.longitude
-    end
-
-    @events = Event.featured(visitor_latitude, visitor_longitude)
+    # Enable if we want to feature events nearby
+    
+    # # Test geocoding services locally by hardcoding latitude and longitude
+    # if Rails.env.development? || Rails.env.test?
+    #   visitor_latitude = 34.05223
+    #   visitor_longitude = -118.24368
+    # else
+    #   visitor_latitude = request.location.latitude
+    #   visitor_longitude = request.location.longitude
+    # end
+    @events = []
   end
 
   def about
