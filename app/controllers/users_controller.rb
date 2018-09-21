742cad6f5853128238f40a9b5607706fa424d389
Paul Wenig <pwenig@gmail.com>
2018-09-21 17:18:13 -0600

    Add redirect after login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      redirect_to @user
+      redirect_to session[:previous_url] || @user
     else
       render :new
     end
