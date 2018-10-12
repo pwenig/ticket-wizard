614ded54ee660ab802bae9a9cacc7a0c51f07c3d
Paul Wenig <pwenig@gmail.com>
2018-10-11 20:01:40 -0600

    Add env vars

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,6 +49,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.asset_host = "http://localhost:3000"
   config.action_mailer.smtp_settings = {
     address:              "smtp.gmail.com",
     port:                 587,
