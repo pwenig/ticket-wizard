2835cab59964d8782bbe875d1bbd52788db12151
Paul Wenig <pwenig@gmail.com>
2018-11-23 13:12:27 -0700

    Comment out carrier wave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      # Configuration for Amazon S3
-      provider: "AWS",
-      aws_access_key_id: ENV["AWS_ACCESS_KEY"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: "us-west-2"
-    }
-    config.fog_directory = ENV["AWS_BUCKET"]
-  end
-end
+# if Rails.env.production?
+#   CarrierWave.configure do |config|
+#     config.fog_credentials = {
+#       # Configuration for Amazon S3
+#       provider: "AWS",
+#       aws_access_key_id: ENV["AWS_ACCESS_KEY"],
+#       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+#       region: "us-west-2"
+#     }
+#     config.fog_directory = ENV["AWS_BUCKET"]
+#   end
+# end
