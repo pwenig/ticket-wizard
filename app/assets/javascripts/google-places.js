d3323ada7ab79c620a64118ac874ac617b4691e8
Paul Wenig <pwenig@gmail.com>
2018-11-30 16:34:14 -0700

    Move JS

--- app/assets/javascripts/google-places.js
+++
@@ -1,4 +0,0 @@
-google.maps.event.addDomListener(window, 'load', initialize);
-function initialize() {
-  var autocomplete = new google.maps.places.Autocomplete(document.getElementById("txtautocomplete"));
-};
