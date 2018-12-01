7e5b36db8ccdce444928dffe2122bed8b2427ca7
Paul Wenig <pwenig@gmail.com>
2018-12-01 09:22:17 -0700

    Remove else - not needed

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -12,8 +12,6 @@ class DashboardController < ApplicationController
       @event_time_remaining = helpers.distance_of_time_in_words_to_now(@event.date)
       @ticket_types = Dashboard.calculate_ticket_types(@event)
       @tickets_by_day = PurchasedTicket.where(event_id: @event.id).group_by_day(:created_at).count
-    else 
-      redirect_to root_path
     end 
   end
 end 
