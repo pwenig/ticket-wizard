d9b84386bbd441e8a93e39a5323296efca3faea2
Paul Wenig <pwenig@gmail.com>
2018-11-30 17:18:51 -0700

    Move methods to model, added specs

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,27 +3,10 @@ class DashboardController < ApplicationController
 
   def index
     @event = Event.where(id: params[:event_id], event_guid: params[:key]).first
-    @event_sales_volume = calculate_volume(@event)
-    @event_tickets = calculate_tickets_sold(@event)
+    @event_sales_volume = Dashboard.calculate_volume(@event)
+    @event_tickets = Dashboard.calculate_tickets_sold(@event)
     @event_time_remaining = helpers.distance_of_time_in_words_to_now(@event.date)
-    @ticket_types = calculate_ticket_types(@event)
+    @ticket_types = Dashboard.calculate_ticket_types(@event)
     @tickets_by_day = PurchasedTicket.where(event_id: @event.id).group_by_day(:created_at).count
   end
-
-  private
-
-  def calculate_volume(event)
-    PurchasedTicket.where(event_id: event.id).map{|x| x.ticket}.map{|c| c.price}.inject(0){|sum,x| sum + x }
-  end
-  
-  def calculate_tickets_sold(event)
-    PurchasedTicket.where(event_id: event.id).length
-  end 
-
-  def calculate_ticket_types(event)
-    tickets_purchased = PurchasedTicket.where(event_id: event.id).map{|x| x.ticket}.group_by{|x| x.title}
-    tickets_purchased.map { |key, value| [key, value.length] }.to_h
-  end 
-
-
 end 
---
+++ spec/models/dashboard_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe Dashboard, type: :model do
+  let(:category1) { create(:category) }
+  let(:user) { create(:user) }
+  let!(:event) { create(:event, date: "#{Time.now + 2.week.to_i}", category_id: category1.id) }
+  let(:ticket) { create(:ticket, title: "VIP", price: 100, event_id: event.id) }
+  let(:ticket2) { create(:ticket, title: "GA", price: 250, event_id: event.id) }
+
+  before do
+    FactoryBot.create_list(:purchased_ticket, 10, user_id: user.id, event_id: event.id, ticket_id: ticket.id )
+    FactoryBot.create_list(:purchased_ticket, 10, user_id: user.id, event_id: event.id, ticket_id: ticket2.id )
+  end
+
+  it "calculates the ticket sales volume for an event by calling #calculate_volume" do 
+    result = Dashboard.calculate_volume(event)
+    expect(result).to eq(3500)
+  end 
+
+  it "calculates the number of tikcets sold for an event by calling #calculate_tickets_sold" do
+    result = Dashboard.calculate_tickets_sold(event)
+    expect(result).to eq(20)
+  end
+
+  it "returns a hash with ticket type and qty sold by calling #calculate_ticket_types" do 
+    result = Dashboard.calculate_ticket_types(event)
+    expect(result.class).to eq(Hash)
+    expect(result["VIP"]).to eq(10)
+    expect(result["GA"]).to eq(10)
+  end 
+end 
