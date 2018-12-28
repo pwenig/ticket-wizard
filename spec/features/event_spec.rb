b45c74756e7b7685137ecb6ee20cc9e085cfdde0
Paul Wenig <pwenig@gmail.com>
2018-12-28 09:59:43 -0700

    Add icons

--- spec/features/event_spec.rb
+++ spec/features/event_spec.rb
@@ -61,10 +61,12 @@ RSpec.describe "Events", type: :feature do
   it "should not edit an event with invalid date" do
     current_day = Date.today.strftime("%d")
     current_month = Date.today.strftime("%B")
+    current_year = Date.today.strftime("%Y")
     visit edit_event_path(upcoming_event, key: upcoming_event.event_guid)
     expect(page.text).to include("Edit Event")
     select(current_month, from: "event_date_2i")
     select(current_day.to_i, from: "event_date_3i")
+    select(current_year, from: "event_date_1i")
     click_on "edit_button"
     expect(page.text).to include("Date must be 1 or more days ahead from now")
   end
